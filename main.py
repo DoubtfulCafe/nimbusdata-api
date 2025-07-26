a3e595a6604761473e507d53e090215adcec173b
Daniel Villeda <dvilleda2007@gmail.com>
2025-07-26 01:27:15 -0600

    feat/authentication: conectar Firebase y crear servicios de registro e inicio de sesión de usuarios

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from utils.database import get_db_connection
 import logging
 
+from controllers.firebase import register_user_firebase, login_user_firebase
+from models.Userlogin import UserRegister
+
 app = FastAPI()
 logger = logging.getLogger(__name__)
 
@@ -26,3 +29,11 @@ async def test_db_connection():
     except Exception as e:
         logger.error(f"Error al conectar a la base de datos: {str(e)}")
         return {"error": "❌ Fallo la conexión con la base de datos", "details": str(e)}
+
+@app.post("/register")
+async def register(user: UserRegister):
+    return await register_user_firebase(user)
+
+@app.post("/login/custom")
+async def login_customer(user: UserRegister):
+    return await login_user_firebase(user)
