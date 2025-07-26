b999864c0f61c1bfef3194638095de8a7205cb07
Daniel Villeda <dvilleda2007@gmail.com>
2025-07-26 05:40:14 -0600

    refactor: conexion con firebase, base de datos y manipulacion de archivos

--- controllers/firebase.py
+++ controllers/firebase.py
@@ -1,62 +1,99 @@
 import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
+import json
+import logging
 import firebase_admin
-from firebase_admin import credentials, auth as firebase_auth
 import requests
-from models.Userlogin import UserRegister
 
-load_dotenv()
+from fastapi import HTTPException
+from firebase_admin import credentials, auth as firebase_auth
+from dotenv import load_dotenv
+
+
+from utils.database import execute_query_json
+from utils.security import create_jwt_token
+from models.Userregister import UserRegister
+from models.Userlogin import UserLogin
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Inicializar la app de Firebase Admin
 cred = credentials.Certificate("secrets/firebase-adminsdk.json")
 firebase_admin.initialize_app(cred)
 
-async def register_user_firebase(user: UserRegister):
+load_dotenv()
+
+async def register_user_firebase(user: UserRegister) -> dict:
+    user_record = {}
     try:
-        # Crear usuario en Firebase Authentication
         user_record = firebase_auth.create_user(
             email=user.email,
             password=user.password
         )
-        return {
-            "message": "Usuario registrado exitosamente",
-            "user_id": user_record.uid
-        }
+
     except Exception as e:
+        print(e)
         raise HTTPException(
             status_code=400,
             detail=f"Error al registrar usuario: {e}"
         )
 
-async def login_user_firebase(user: UserRegister):
+    query = f" exec epic.users_insert ?, ?, ?, ? "
+    params = (
+        user_record.email,
+        user.first_name,
+        user.last_name,
+        user.active
+    )
     try:
-        # Autenticar usuario con Firebase Authentication usando la API REST
-        api_key = os.getenv("FIREBASE_API_KEY")  # Reemplaza esto con tu apiKey de Firebase
-        url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
-        payload = {
-            "email": user.email,
-            "password": user.password,
-            "returnSecureToken": True
-        }
-
-        response = requests.post(url, json=payload)
-        response_data = response.json()
-
-        if "error" in response_data:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Error al autenticar usuario: {response_data['error']['message']}"
-            )
-
-        id_token = response_data["idToken"]
-        return {
-            "message": "Usuario autenticado exitosamente",
-            "idToken": id_token
-        }
-
+        result_json = await execute_query_json(query, params, needs_commit=True)
+        return json.loads(result_json)
     except Exception as e:
+        firebase_auth.delete_user(user_record.uid)
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def login_user_firebase(user: UserLogin):
+    # Autenticar usuario con Firebase Authentication usando la API REST
+    api_key = os.getenv("FIREBASE_API_KEY")  # Reemplaza esto con tu apiKey de Firebase
+    url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
+    payload = {
+        "email": user.email,
+        "password": user.password,
+        "returnSecureToken": True
+    }
+    response = requests.post(url, json=payload)
+    response_data = response.json()
+
+    if "error" in response_data:
         raise HTTPException(
             status_code=400,
-            detail=f"Error al autenticar usuario: {e}"
+            detail=f"Error al autenticar usuario: {response_data['error']['message']}"
         )
+
+    query = f"""select
+                    email
+                    , firstname
+                    , lastname
+                    , active
+                    , [admin]
+                from [epic].[users]
+                where email = ?
+                """
+
+    try:
+        result_json = await execute_query_json(query, (user.email,), needs_commit=False)
+        result_dict = json.loads(result_json)
+        return {
+            "message": "Usuario autenticado exitosamente",
+            "idToken": create_jwt_token(
+                result_dict[0]["firstname"],
+                result_dict[0]["lastname"],
+                user.email,
+                result_dict[0]["active"],
+                result_dict[0]["admin"],
+            )
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Response, Request
+import uvicorn
 from utils.database import get_db_connection
 import logging
 
 from controllers.firebase import register_user_firebase, login_user_firebase
-from models.Userlogin import UserRegister
+from models.Userlogin import UserLogin
+from models.Userregister import UserRegister
+
+from utils.security import validate
 
 app = FastAPI()
 logger = logging.getLogger(__name__)
 
 
 @app.get("/")
-def read_root():
-    """
-    Endpoint de prueba para verificar que la API está corriendo.
-    """
-    return {"message": "API NimbusData corriendo correctamente 🚀"}
+@validate
+async def read_root(request: Request, response: Response):
+    return {
+        "hello": "world"
+    }
+
 
 
 @app.get("/test-db")
@@ -30,10 +36,15 @@ async def test_db_connection():
         logger.error(f"Error al conectar a la base de datos: {str(e)}")
         return {"error": "❌ Fallo la conexión con la base de datos", "details": str(e)}
 
-@app.post("/register")
-async def register(user: UserRegister):
-    return await register_user_firebase(user)
+@app.post("/signup")
+async def signup(user: UserRegister):
+    result = await register_user_firebase(user)
+    return result
 
-@app.post("/login/custom")
-async def login_customer(user: UserRegister):
-    return await login_user_firebase(user)
+@app.post("/login")
+async def login(user: UserLogin):
+    result = await login_user_firebase(user)
+    return result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
